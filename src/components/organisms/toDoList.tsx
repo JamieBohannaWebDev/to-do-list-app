8e5b6371e4d3489f33a6bdbfab6754452eeace80
Jamie Bohanna <jamiebohanna.webdev@gmail.com>
2023-10-06 18:47:58 +0100

    feat: added mainly empty todo list component

---
+++ src/components/organisms/toDoList.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ToDoList = () => {
+  return (
+    <main className='flex w-full justify-center flex-col items-center'>
+      <div className='flex'><h1>Today</h1><p>5</p></div>
+      
+      {/* Add new task component */}
+
+      {/* Item component */}
+
+      {/* Completed tasks component */}
+    </main>
+  )
+}
+
+export default ToDoList
