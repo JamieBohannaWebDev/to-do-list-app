810f0f06c6a553eff3ca439d48ad35069a50cca7
Jamie Bohanna <jamiebohanna.webdev@gmail.com>
2023-10-07 12:28:48 +0100

    style: wee styling tweak to make text look nicer

--- src/components/organisms/sideMenu.tsx
+++ src/components/organisms/sideMenu.tsx
@@ -5,7 +5,7 @@ import SettingsMenu from '../molecules/settingsMenu';
 
 const SideMenu = () => {
 	return (
-		<section className="w-1/3 max-w-[265px] flex flex-col border h-screen border-red-100 p-5">
+		<section className="w-1/3 max-w-[265px] flex flex-col border h-screen text-slate-600 bg-[#F4F4F4]  p-5">
 			<h2 className="mb-10 text-2xl font-bold">Menu</h2>
 			<div className="flex justify-between flex-col grow">
         <div>
